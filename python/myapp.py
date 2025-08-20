f915ae4132395cced97653661ba64ec2135e3039
nikhita-code <nikhita.kalamkar@gmail.com>
2025-08-20 22:27:21 +0530

    Create myapp.py

---
+++ python/myapp.py
@@ -0,0 +1,11 @@
+# Program to add two numbers with user input
+
+# Taking input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculating sum
+result = num1 + num2
+
+# Displaying the result
+print("The sum is:", result)
